2b4963bb4afa23d38311b5acef4ae23a947f5d85
Nguyen Thanh Tuan <tuannt.ti.1922@gmail.com>
2025-05-19 15:07:57 +0700

    Setup script and update README for the initial setup

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # spring-petclinic-microservices-k8s-config
 
 This is a repository storing configuration files for K8S cluster using ArgoCD.
+
+## Initial Setup Instruction
+Prerequisite: 
+- Using a Linux distro.
+- Already installed helm and connected to a K8S cluster.
+
+Installation:
+```bash
+./initial-setup.sh
+```
---
+++ initial-setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# This script sets up the initial environment for the K8S cluster.
+
+# Nginx Ingress Controller Setup Script
+echo "Setting up Nginx Ingress Controller..."
+helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
+helm repo update
+helm install ingress-nginx ingress-nginx/ingress-nginx \
+  --namespace ingress-nginx \
+  --create-namespace
+echo "Nginx Ingress Controller setup complete."
+
+# ArgoCD Setup Script
+echo "Setting up ArgoCD..."
+helm repo add argo https://argoproj.github.io/argo-helm
+helm repo update
+helm install argocd argo/argo-cd \
+  --namespace argocd \
+  --create-namespace \
+  --set server.service.type=LoadBalancer
+echo "ArgoCD setup complete."
